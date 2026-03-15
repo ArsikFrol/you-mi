2aa09fcd3a3a55b15aca27d0757cad6cf9ea9f95
ArseniyFrolov <arsik.frol.555@gmail.com>
2026-03-15 12:14:04 +0500

    fix: showNav

--- src/app/messages/layout.tsx
+++ src/app/messages/layout.tsx
@@ -12,7 +12,7 @@ export default function TariffLayout({
 }>) {
     return (
         <>
-            <Header notShowNav white />
+            <Header showNav white />
             {children}
         </>
     );
